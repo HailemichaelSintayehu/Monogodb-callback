78016820a8ab17dd96a3648f03cd9c810c301778
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-08-31 12:04:49 +0300

    callback hell and promise

---
+++ index.js
@@ -0,0 +1,57 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const dboper = require('./operations');
+
+const url = 'mongodb://localhost:27017/';
+
+const dbname = 'conFusion';
+
+// MongoClient.set('useNewUrlParser', true);
+
+// MongoClient.set('useUnifiedTopology', true);
+
+MongoClient.connect(url)
+    .then((client)=>{
+
+        console.log("Connected correctly to the server");
+
+        const db = client.db(dbname);
+
+        dboper.insertDocument(db,{name: "Vadonut",description:"Testy"},'dishes')
+    .then((result)=>{
+
+            console.log("Insert Document:\n",result);
+
+            return dboper.findDocuments(db,'dishes')
+
+    })
+    .then((docs)=>{
+
+        console.log("Found Documents:\n" ,docs);
+
+        return dboper.updateDocument(db,{name:"Vadonut"},{description:"Updated Test"},'dishes')
+    })
+    .then((result)=>{
+
+        console.log("Updated Document:\n",result.result);
+
+        return dboper.findDocuments(db,'dishes')
+    })
+    .then((docs)=>{
+
+        console.log("Found Documents: \n", docs);
+
+        return db.dropCollection('dishes')
+    })
+    .then((result)=>{
+
+        console.log("Dropped Collection: " ,result);
+
+        client.close();
+
+    })
+    .catch((err)=>console.log(err));
+    
+})
+.catch((err)=>console.log(err));
+
